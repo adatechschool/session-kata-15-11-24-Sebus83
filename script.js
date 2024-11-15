62ed1fc9168e3eb4388aa9559be0893cdef97b94
Sebus83 <sebastien.herbiet@gmail.com>
2024-11-15 10:51:10 +0100

    etape 1

---
+++ script.js
@@ -0,0 +1,11 @@
+function getLatinCharacterList(texte){ //je crée une fonction avec un paramètre texte
+
+    let tableauTexte = texte.split("") // je crée une variable pour alimenter un tableau avec chaque caractère de mon paramètre texte
+    
+    
+    console.log(tableauTexte) //j'affiche mon tableau dans la console
+ 
+  
+}
+
+getLatinCharacterList("Hello, World") //j'appelle ma fonction avec l'argument "Hello, World"
